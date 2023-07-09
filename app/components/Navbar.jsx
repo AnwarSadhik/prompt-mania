54377e7240d719282b93583563b0ef6d934adc3b
AnwarSadhik <anwarsdhk@gmail.com>
2023-07-09 18:13:42 +0530

    Landing DONE (dance emoji)

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { SiProbot } from "react-icons/si";
 
 export default function Navbar() {
   return (
-    <div className="w-full py-4 px-3 md:px-12 sticky top-0 left-0 right-0 z-10">
+    <div className="w-full py-4 px-3 md:px-12 fixed top-0 left-0 right-0 z-10">
       <div className="flex justify-between items-center">
         <div className="flex items-center md:px-8 gap-8">
             <SiProbot size={40} color="black" className="mb-2"/>
